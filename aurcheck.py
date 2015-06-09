e7a03f7f5c7ca3867b669f5c92af685afd1078d4
Charles Bos <charlesbos1@gmail.com>
2015-06-09 22:39:15 +0100

    remove --strict option for AUR 4, it is now default. change output presentation slightly. add seperate category for not migrated AUR 3 pkgs

--- aurcheck.py
+++ aurcheck.py
@@ -11,8 +11,7 @@ import sys
 args = sys.argv
 if ('-h' in args) or ('--help' in args) : print('''Usage:
   -h, --help: Print this help message
-  -aur4: Check the new git based AUR instead of the current AUR 3
-  --strict: AUR 4 option. Assume that untransitioned packages are not in AUR 4''')
+  -aur4: Check the new git based AUR 4 instead of the current AUR 3''')
 else :
     print("Checking...")
     
@@ -32,36 +31,42 @@ else :
         updates = []
         mismatches = []
         failures = []
+        aur3Pkgs = []
+        
         if aurVer == 4 : aurUrl = "https://aur4.archlinux.org/packages/"
         else : aurUrl = "https://aur.archlinux.org/packages/"
         
         for x in aurPkgs:
-            if (aurVer == 4) and ('--strict' in args) :
-                response = requests.get("https://aur4.archlinux.org/cgit/aur.git/log/?h=" + x[0])
-                existCheck = str(BeautifulSoup(response.content))
-                if existCheck.find("Invalid branch: " + x[0]) != -1 : failures.append("Check for " + x[0] + " failed. Is it in the AUR?")
-            else :    
-                response = requests.get(aurUrl + x[0])
-                page = str(BeautifulSoup(response.content))
-                start = page.find("<h2>Package Details: ") + 21
-                if start == 20 : failures.append("Check for " + x[0] + " failed. Is it in the AUR?")
-                else :
-                    end = page.find("</h2>", start)
-                    version = page[start:end].split(" ")[1]
-                    if x[1] < version : updates.append((x[0], str("Update for " + x[0] + " is available: " + x[1] + " --> " + version)))
-                    elif x[1] > version : mismatches.append(str("Local " + x[0] + " looks newer than AUR version: " + x[1] + " --> " + version))
+            response = requests.get(aurUrl + x[0])
+            page = str(BeautifulSoup(response.content))
+            start = page.find("<h2>Package Details: ") + 21
+            if start == 20 : failures.append(x[0])
+            else :
+                if aurVer == 4 :
+                    response = requests.get("https://aur4.archlinux.org/cgit/aur.git/log/?h=" + x[0])
+                    logPage = str(BeautifulSoup(response.content))
+                    if logPage.find("Invalid branch: " + x[0]) != -1 :
+                        aur3Pkgs.append(x[0])
+                        continue
+                end = page.find("</h2>", start)
+                version = page[start:end].split(" ")[1]
+                if x[1] < version : updates.append(str(x[0] + " " +x[1] + " --> " + version))
+                elif x[1] > version : mismatches.append(str(x[0] + " " +x[1] + " --> " + version))  
 
-        if updates == mismatches == failures == [] : print("\nEverything is up to date.")
+        if updates == mismatches == failures == aur3Pkgs == [] : print("\nEverything is up to date.")
         else :
             if failures != [] :
-                print("\nSearch failures:\n----------------")
+                print("\nThe following packages were not found in the AUR:\n-----")
                 for x in failures : print(x)
+            if aur3Pkgs != [] :
+                print("\nThe following packages are in AUR 3 but not AUR 4:\n-----")
+                for x in aur3Pkgs : print(x)
             if mismatches != [] :
-                print("\nLocal remote mismatches:\n------------------------")
+                print("\nThe following local packages are more recent than their AUR versions:\n-----")
                 for x in mismatches : print(x)
             if updates != [] :
-                print("\nAvailable updates:\n------------------")
-                for x in updates : print(x[1])
+                print("\nThe following packages have updates available:\n-----")
+                for x in updates : print(x)
                 fetch = input("\nFetch updated tarballs? [y/n] ")
                 if fetch == "y" :
                     if os.path.exists(os.path.expanduser('~') + "/Downloads") : downloadsDir = os.path.expanduser('~') + "/Downloads"
