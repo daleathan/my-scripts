424f2516b9d44cdaad2878893ba33cf7bc399f06
Charles Bos <charlesbos1@gmail.com>
2015-06-13 15:21:01 +0100

    do not tie operation to whether gnome-screesaver is running. instead, add check at start to ensure script isn't started multiple times

--- gnome-screensaver-idlelock.sh
+++ gnome-screensaver-idlelock.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-# Lock screen with GNOME Screensaver when the monitor is switched off
-# Note: works only if monitor is switched off, not if monitor is blanked
+#Lock screen with GNOME Screensaver when the monitor is switched off
+#Note: works only if monitor is switched off, not if monitor is blanked
+
+#Check if already running and exit immediately if so
+RUNNING=$(ps aux | grep gnome-screensaver-idlelock.sh | grep -v grep | wc -l)
+if [ "$RUNNING" -gt "2" ]; then
+  echo "gnome-screeensaver-idlelock.sh is already running"
+  exit 0
+fi
  
 while true
 do
-  if [ "$(pidof gnome-screensaver)" ]; then
-    MONITOR=$(xset -q | grep Monitor)
-    SCREENSAVER=$(gnome-screensaver-command -q)
-    if [ "$MONITOR" = "  Monitor is Off" ] && [ "$SCREENSAVER" = "The screensaver is inactive" ]; then
-      gnome-screensaver-command --lock
-    fi
-    sleep 1
-  else
-    exit 0
+  MONITOR=$(xset -q | grep Monitor)
+  SCREENSAVER=$(gnome-screensaver-command -q)
+  if [ "$MONITOR" = "  Monitor is Off" ] && [ "$SCREENSAVER" = "The screensaver is inactive" ]; then
+    gnome-screensaver-command --lock
   fi
+  sleep 1
 done
