cfd381d0ab7d13e8597b727a347001d5bc506d03
Charles Bos <charlesbos1@gmail.com>
2015-06-23 23:18:27 +0100

    mcityconf.py - allow individual settings to be reset to default. remove time.sleep. wait for provess to complete instead

--- mcityconf.py
+++ mcityconf.py
@@ -5,7 +5,6 @@
 from tkinter import *
 from subprocess import Popen, PIPE
 import os
-import time
 
 def getTheme() :
     theme = Popen(["gsettings", "get", "org.gnome.metacity", "theme"], stdout = PIPE).communicate()
@@ -75,21 +74,30 @@ def setButtonLayout() :
     layout = e2.get()
     Popen(["gsettings", "set", "org.gnome.desktop.wm.preferences", "button-layout", layout])
 
-def reset() :
-    #Reset settings
-    Popen(["gsettings", "reset", "org.gnome.metacity", "theme"])
-    Popen(["gsettings", "reset", "org.gnome.desktop.wm.preferences", "titlebar-uses-system-font"])
-    Popen(["gsettings", "reset", "org.gnome.desktop.wm.preferences", "titlebar-font"])
-    Popen(["gsettings", "reset", "org.gnome.desktop.wm.preferences", "button-layout"])
-    
-    #Update ui
-    time.sleep(0.5)
-    varOp.set("Current: " + getTheme())
-    b1["text"] = "Toggle (Now on)"
-    e1.delete(first = 0, last = len(e1.get()) + 1)
-    e2.delete(first = 0, last = len(e2.get()) + 1)
-    e1.insert(0, getTitleFont())
-    e2.insert(0, getButtonLayout())
+class reset() :
+    def theme() :
+        Popen(["gsettings", "reset", "org.gnome.metacity", "theme"]).wait()
+        varOp.set("Current: " + getTheme())
+
+    def useSysFont() :
+        Popen(["gsettings", "reset", "org.gnome.desktop.wm.preferences", "titlebar-uses-system-font"]).wait()
+        b1["text"] = "Toggle (Now on)"
+
+    def titlebarFont() :
+        Popen(["gsettings", "reset", "org.gnome.desktop.wm.preferences", "titlebar-font"]).wait()
+        e1.delete(first = 0, last = len(e1.get()) + 1)
+        e1.insert(0, getTitleFont())
+
+    def buttonLayout() :
+        Popen(["gsettings", "reset", "org.gnome.desktop.wm.preferences", "button-layout"]).wait()
+        e2.delete(first = 0, last = len(e2.get()) + 1)
+        e2.insert(0, getButtonLayout())
+
+    def All() :
+        reset.theme()
+        reset.useSysFont()
+        reset.titlebarFont()
+        reset.buttonLayout()
         
 class UI() :
     def __init__(self, parent) :
@@ -122,15 +130,38 @@ class UI() :
         b2 = Button(parent, text = "Update", command = setTitleFont).grid(row = 4, column = 3)
 
         #Button layout ui
-        l4 = Label(parent, text = "Set button layout:", pady = 7, padx = 5).grid(row = 5, column = 1)
+        l5 = Label(parent, text = "Set button layout:", pady = 7, padx = 5).grid(row = 5, column = 1)
         global e2
         e2 = Entry(parent)
         e2.grid(row = 5, column = 2)
         e2.insert(0, getButtonLayout())
         b3 = Button(parent, text = "Update", command = setButtonLayout).grid(row = 5, column = 3)
 
-        #Reset all
-        b4 = Button(parent, text = "Reset all", command = reset).grid(row = 6, column = 3)
+        #Reset
+        b4 = Button(parent, text = "Reset...", command = lambda : resetWindow()).grid(row = 6, column = 3)
+
+        def runReset() :
+            if resetVar.get() == 1 : reset.theme()
+            elif resetVar.get() == 2 : reset.useSysFont()
+            elif resetVar.get() == 3 : reset.titlebarFont()
+            elif resetVar.get() == 4 : reset.buttonLayout()
+            elif resetVar.get() == 5 : reset.All()
+            Toplevel.destroy(resetWin)
+
+        def resetWindow() :
+            global resetWin
+            resetWin = Toplevel()
+            resetWin.title("Choose option")
+            l6 = Label(resetWin, text = "What would you like to reset?", pady = 5, padx = 150, relief = RAISED)
+            l6.grid(row = 1, column = 1, columnspan = 5)
+            global resetVar
+            resetVar = IntVar()
+            r1 = Radiobutton(resetWin, text = "Theme", variable = resetVar, value = 1).grid(row = 2, column = 1)
+            r2 = Radiobutton(resetWin, text = "Use system font", variable = resetVar, value = 2).grid(row = 2, column = 2)
+            r3 = Radiobutton(resetWin, text = "Titlebar font", variable = resetVar, value = 3).grid(row = 2, column = 3)
+            r4 = Radiobutton(resetWin, text = "Button layout", variable = resetVar, value = 4).grid(row = 2, column = 4)
+            r5 = Radiobutton(resetWin, text = "All", variable = resetVar, value = 5).grid(row = 2, column = 5)
+            b5 = Button(resetWin, text = "Ok", command = runReset).grid(row = 3, column = 2, columnspan = 3)
         
 top = Tk()
 top.title("Settings for Metacity")
