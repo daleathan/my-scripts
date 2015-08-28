4b4cc3350dc7988e822686f76914fbaf2f2db313
Charles Bos <charlesbos1@gmail.com>
2015-08-28 14:07:56 +0100

    gnome-screensaver-idlelock.sh - add support for the xfpm presentation mode

--- gnome-screensaver-idlelock.sh
+++ gnome-screensaver-idlelock.sh
@@ -13,8 +13,15 @@ while true
 do
   IDLE=$(xprintidle)
   SCREENSAVER=$(gnome-screensaver-command -q)
-  if [ "$IDLE" -ge 600000 ] && [ "$SCREENSAVER" = "The screensaver is inactive" ]; then
-    gnome-screensaver-command --lock
+  if [ "$IDLE" -ge 10000 ] && [ "$SCREENSAVER" = "The screensaver is inactive" ]; then
+    if [ $(pgrep -f xfce4-power-manager) ]; then
+      PRESENTATION_MODE=$(xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/presentation-mode)
+      if [ "$PRESENTATION_MODE" = "false" ]; then
+        gnome-screensaver-command --lock
+      fi
+    else
+      gnome-screensaver-command --lock
+    fi
   fi
   sleep 1
 done
