dcafa9ea6918ffa2b68b06506db3721378e575d0
Charles Bos <charlesbos1@gmail.com>
2015-09-08 13:33:10 +0100

    mp3-files-renamer.py - add fix for illegal characters

--- mp3-files-renamer.py
+++ mp3-files-renamer.py
@@ -31,11 +31,11 @@ ignore = []
 for root, dirs, files in os.walk(musicDir, topdown=False):
     for name in files:
         if name.find(".mp3") != -1 : 
-            musicFiles.append((root, os.path.join(root, name)))
+            musicFiles.append((root, name))
 
 for x in musicFiles :
     try :
-        audio = ID3(x[1])
+        audio = ID3(os.path.join(x[0], x[1]))
         title = str(audio["TIT2"].text[0])
         trackNum = str(audio["TRCK"].text[0])
         convList.append([trackNum, title])
@@ -44,12 +44,6 @@ for x in musicFiles :
 
 #Remove problem files from list
 for x in ignore : musicFiles.remove(x)
-
-#Add leading 0 if missing
-#Remove number of tracks per album if it exists
-for x in convList :
-    if len(x[0].split("/")[0]) == 1 : convList[convList.index(x)][0] = "0" + x[0]
-    convList[convList.index(x)][0] = x[0].split("/")[0]
 
 #If no files to convert, report problem files and exit
 if len(convList) == 0 :
@@ -60,10 +54,27 @@ if len(convList) == 0 :
         for x in ignore : print(x[1])
     os._exit(0)
 else :
-    #Start renaming
+    #Add leading 0 if missing
+    #Remove number of tracks per album if it exists
+    for x in convList :
+        if len(x[0].split("/")[0]) == 1 : convList[convList.index(x)][0] = "0" + x[0]
+        convList[convList.index(x)][0] = x[0].split("/")[0]
+
+    #Fix illegal characters
+    counter = 0
+    while counter < len(musicFiles) :
+        if sys.platform == "win32" :
+            winIllegals = ["<", ">", ":", '"', "/", "\\", "|", "?", "*"]
+            for x in convList[counter][1] : 
+                if x in winIllegals : convList[counter][1].replace(x, "_")
+        else :
+            if convList[counter][1].find("/") != -1 : convList[counter][1] = convList[counter][1].replace("/", "_")
+        counter += 1
+
+    #Rename files
     counter = 0
     for x in musicFiles :
-        os.rename(x[1], os.path.join(x[0], convList[counter][0] + " " + convList[counter][1] + ".mp3"))
+        os.rename(os.path.join(x[0], x[1]), os.path.join(x[0], convList[counter][0] + " " + convList[counter][1] + ".mp3"))
         counter += 1
     print(str(len(convList)) + " files were successfully renamed.")
     if len(ignore) > 0 :
