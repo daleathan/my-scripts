ac4f178707cd1edb055637786eea115dec8fa227
Charles Bos <charlesbos1@gmail.com>
2015-10-17 19:25:39 +0100

    xfpm-idle-toggle.sh - time limit on notify-send command

--- xfpm-idle-toggle.sh
+++ xfpm-idle-toggle.sh
@@ -5,8 +5,8 @@ QUERY="xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/presentation-
 PRES_MODE=$(eval $QUERY)
 if [ $PRES_MODE = "false" ]; then
   eval "$QUERY -s true"
-  notify-send "Presentation mode: ON" -i xfpm-ac-adapter
+  notify-send "Presentation mode: ON" -i xfpm-ac-adapter -t 2000
 else
   eval "$QUERY -s false"
-  notify-send "Presentation mode: OFF" -i xfpm-ac-adapter
+  notify-send "Presentation mode: OFF" -i xfpm-ac-adapter -t 2000
 fi
