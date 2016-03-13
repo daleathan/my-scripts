0aded72b8e5bf4d988c1b8c8d105bc76452f899a
Charles Bos <charlesbos1@gmail.com>
2016-03-13 09:16:03 +0000

    volume-change.sh - remove unneeded variables and sh -c

--- volume-change.sh
+++ volume-change.sh
@@ -6,24 +6,23 @@ usage() {
   volume-change.sh [OPTIONS]
 
 Options:
-  -h, --help: show this dialogue
   up: increase the volume by 5%
   down: decrease the volume by 5%
   toggle: mute/unmute the volume"
 }
 
-if [ ! "$1" ] || [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
-  usage
-elif [ "$1" == "toggle" ]; then
+if [ "$1" == "toggle" ]; then
   pactl set-sink-mute $(pactl list sinks short | cut -f 1) toggle
 elif [ "$1" == "up" ]; then
-  CURRENT_VOLUME=$(amixer | grep "Front Left: Playback" | cut -d "[" -f 2 | cut -d "%" -f 1)
-  if [ "$CURRENT_VOLUME" -lt 100 ]; then
-    sh -c 'pactl set-sink-mute $(pactl list sinks short | cut -f 1) false; pactl set-sink-volume $(pactl list sinks short | cut -f 1) +5%'
+  if [ $(amixer | grep "Front Left: Playback" | cut -d "[" -f 2 | cut -d "%" -f 1) -lt 100 ]; then
+    pactl set-sink-mute $(pactl list sinks short | cut -f 1) false
+    pactl set-sink-volume $(pactl list sinks short | cut -f 1) +5%
   fi
 elif [ "$1" == "down" ]; then
-  CURRENT_VOLUME=$(amixer | grep "Front Left: Playback" | cut -d "[" -f 2 | cut -d "%" -f 1)
-  if [ "$CURRENT_VOLUME" -gt 0 ]; then
-    sh -c 'pactl set-sink-mute $(pactl list sinks short | cut -f 1) false; pactl set-sink-volume $(pactl list sinks short | cut -f 1) -5%'
+  if [ $(amixer | grep "Front Left: Playback" | cut -d "[" -f 2 | cut -d "%" -f 1) -gt 0 ]; then
+    pactl set-sink-mute $(pactl list sinks short | cut -f 1) false 
+    pactl set-sink-volume $(pactl list sinks short | cut -f 1) -5%
   fi
+else
+  usage
 fi
