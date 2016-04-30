37362b25189a254882632d696ccfcd0b315680ab
Charles Bos <charlesbos1@gmail.com>
2016-04-30 11:51:04 +0100

    add script - fat_copy.sh

---
+++ fat_copy.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+#A script to copy directories and their contents, automatically replacing
+#FAT illegal characters with underscores
+
+usage() {
+  echo "Usage:"
+  echo "  fat_copy.sh src_dir dest_dir"
+  IFS=$IFS_ORIG
+}
+
+IFS_ORIG=$IFS
+IFS=$'\n'
+
+if [ ! $1 ] || [ ! $2 ] || [ ! -d $1 ] || [ ! -d $2 ]; then
+  usage
+  exit
+fi
+
+cd $1
+abs=$(dirname $PWD)
+length=${#abs}
+((length+=1))
+top=${1:$length}
+
+for x in $(find . -type f); do
+  dest=$(echo $2/$top/$(dirname $x) | sed 's@[<>:"|?*]@_@g')
+  if [ ! -d $dest ]; then
+    mkdir -p $dest
+  fi
+  filename=$(basename $x | sed 's@[<>:"/\|?*]@_@g')
+  cp $x $dest/$filename
+done
+
+IFS=$IFS_ORIG
