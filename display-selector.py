d200ff71e8f1ce232ef87b44cc5832c930a3535f
Charles Bos <charlesbos1@gmail.com>
2017-09-10 10:36:16 +0100

    display-selector.py - adjust button titles, internal padding and label background relief

--- display-selector.py
+++ display-selector.py
@@ -21,15 +21,15 @@ class UI() :
         parent.title("Display Selector")
 
         #Display selection
-        l1 = Label(parent, text = "Select the output display(s).", pady = 5, padx = 5).grid(row = 1, column = 1, sticky = W)
+        l1 = Label(parent, text = "Select the output display(s).", pady = 5, padx = 5, relief = RAISED).grid(row = 1, column = 1, sticky = W, pady = (0, 3))
         self.display = IntVar()
         r1 = Radiobutton(parent, text = "Laptop only", variable = self.display, value = 0, pady = 5, padx = 5).grid(row = 2, column = 1, sticky = W)
         r2 = Radiobutton(parent, text = "Television only", variable = self.display, value = 1, pady = 5, padx = 5).grid(row = 3, column = 1, sticky = W)
         r3 = Radiobutton(parent, text = "Laptop and television", variable = self.display, value = 2, pady = 5, padx = 5).grid(row = 4, column = 1, sticky = W)
 
         #Buttons
-        b1 = Button(parent, text = "Cancel", padx = 10, pady = 5, bd = 3, command = parent.destroy).grid(row = 5, column = 1, sticky = W)
-        b2 = Button(parent, text = "OK", padx = 5, pady = 5, bd = 3, command = run).grid(row = 5, column = 1, sticky = E)
+        b1 = Button(parent, text = "Close", padx = 5, pady = 5, bd = 3, command = parent.destroy).grid(row = 5, column = 1, sticky = W, pady = (3, 0))
+        b2 = Button(parent, text = "Select", padx = 5, pady = 5, bd = 3, command = run).grid(row = 5, column = 1, sticky = E, pady = (3, 0))
         
 top = Tk()  
 ui = UI(top)
