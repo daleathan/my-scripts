28bfdd660c5de72bc9b96cd9b3d82876943bc205
Charles Bos <charlesbos1@gmail.com>
2017-12-02 12:00:18 +0000

    display-selector.sh - replaces tabs with two spaces

--- display-selector.sh
+++ display-selector.sh
@@ -4,19 +4,19 @@
 #By Charles Bos
 
 OPTION=$(zenity --title "Display Selector" \
-	--list --text "Select the output display(s)." --radiolist \
-	--column "Select" \
-	--column "Display(s)" \
-	TRUE "Laptop only" \
-	FALSE "Television only" \
-	FALSE "Laptop and television")
+  --list --text "Select the output display(s)." --radiolist \
+  --column "Select" \
+  --column "Display(s)" \
+  TRUE "Laptop only" \
+  FALSE "Television only" \
+  FALSE "Laptop and television")
 
 if [ "$OPTION" = "Television only" ]; then 
-	xrandr --output HDMI1 --auto --output LVDS1 --off
+  xrandr --output HDMI1 --auto --output LVDS1 --off
 elif [ "$OPTION" = "Laptop and television" ]; then
-	xrandr --output HDMI1 --auto --output LVDS1 --auto
+  xrandr --output HDMI1 --auto --output LVDS1 --auto
 else
-	xrandr --output HDMI1 --off --output LVDS1 --auto
+  xrandr --output HDMI1 --off --output LVDS1 --auto
 fi
 
 unset OPTION
