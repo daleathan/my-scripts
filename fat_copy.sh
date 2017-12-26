3c6293342c1f37648964789db5a6fd5222d59f78
Charles Bos <charlesbos1@gmail.com>
2017-12-26 15:29:02 +0000

    fat_copy.sh - allow the source to be a file as well as a directory

--- fat_copy.sh
+++ fat_copy.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
-#A script to copy directories and their contents, automatically replacing
-#FAT illegal characters with underscores. Note that this does NOT work around
+#A script to copy files and directories, automatically replacing illegal
+#characters in FAT with underscores. Note that this does NOT work around
 #FAT's case insensitivity. Use with care!
 #By Charles Bos
 
 usage() {
   echo "Usage:"
-  echo "  fat_copy.sh src_dir dest_dir"
+  echo "  fat_copy.sh src dest"
   IFS=$IFS_ORIG
 }
 
 IFS_ORIG=$IFS
 IFS=$'\n'
 
-if [ ! $1 ] || [ ! $2 ] || [ ! -d $1 ] || [ ! -d $2 ]; then
+if [ ! $1 ] || [ ! $2 ] || ([ ! -d $1 ] && [ ! -f $1 ]) || [ ! -d $2 ]; then
   usage
   exit
 fi
