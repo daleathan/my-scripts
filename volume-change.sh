92c6adb886bbd559699483fa66c50ee5263cc081
Charles Bos <charlesbos1@gmail.com>
2018-03-26 18:47:30 +0100

    volume-change.sh - don't needlessly calculate volume when we're only going to mute/unmute

--- volume-change.sh
+++ volume-change.sh
@@ -11,22 +11,25 @@ Options:
   toggle: mute/unmute the volume"
 }
 
-volume=$(amixer | grep "Front Left: Playback" | cut -d "[" -f 2 | cut -d "%" -f 1)
 
 if [ "$1" == "toggle" ]; then
   pactl set-sink-mute @DEFAULT_SINK@ toggle
 elif [ "$1" == "up" ]; then
+  volume=$(amixer | grep "Front Left: Playback" | cut -d "[" -f 2 | \
+    cut -d "%" -f 1)
   if [ "$volume" -lt 100 ]; then
     pactl set-sink-mute @DEFAULT_SINK@ false
     pactl set-sink-volume @DEFAULT_SINK@ +5%
   fi
+  unset volume
 elif [ "$1" == "down" ]; then
+  volume=$(amixer | grep "Front Left: Playback" | cut -d "[" -f 2 | \
+    cut -d "%" -f 1)
   if [ "$volume" -gt 0 ]; then
     pactl set-sink-mute @DEFAULT_SINK@ false 
     pactl set-sink-volume @DEFAULT_SINK@ -5%
   fi
+  unset volume
 else
   usage
 fi
-
-unset volume
